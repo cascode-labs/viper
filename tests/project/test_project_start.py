f2b0b8af61cd2d62eecbccbd35498f90ce7b9f10
Curtis Mayberry <Curtisma3@gmail.com>
2022-10-21 04:16:44 +0000

    Fix test

--- tests/project/test_project_start.py
+++ tests/project/test_project_start.py
@@ -7,4 +7,6 @@ def test_start_project():
     with patch('subprocess.run') as mock_run:
         start_project('ids_dev')
         mock_run.assert_called_once()
-        mock_run.assert_called_with(["sp_tcsh",'ids_dev',"None", "False", "None"],env=os.environ)
+        mock_run.assert_called_with(
+            ["sp_bash",'ids_dev',"None", "False", "None"],
+            env=os.environ, check=True)
