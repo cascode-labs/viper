f2cd8379324bb4c19c9b558a7408a1dca6c4d0eb
Curtis Mayberry <Curtisma3@gmail.com>
2023-04-20 03:53:20 +0200

    update to have -h help option

--- viper/config/config_data.py
+++ viper/config/config_data.py
@@ -1,7 +1,8 @@
 from typing import Dict, Union, Optional
 from pathlib import Path
-from pydantic import BaseSettings, DirectoryPath, BaseModel, AnyHttpUrl, Extra, FilePath
-
+from pydantic import (BaseSettings, DirectoryPath, BaseModel, AnyHttpUrl, 
+    Extra, FilePath, Field)
+from viper.typing import PathLike
 
 class Conda(BaseModel):
     """Conda environment settings"""
@@ -12,5 +13,6 @@ class ViperConfig(BaseSettings, extra=Extra.ignore):
     """Viper configuration settings"""
     VIPER_CONFIG_PATH: FilePath
     conda: Optional[Conda]
-    default_project_root: DirectoryPath
+    default_project_root: DirectoryPath = Field()
     docs: Dict[str, str]
+    simulation_dirpath: DirectoryPath
