866dd9b73fea177ad5bb1d2a87028047cf3d517a
Curtis Mayberry <Curtisma3@gmail.com>
2024-06-07 21:29:48 -0500

    fix sitedpdks

--- viper/config/oshic_sitepdks.py
+++ viper/config/oshic_sitepdks.py
@@ -2,5 +2,10 @@
 from pathlib import Path
 import sky130
 
-sky130_pdk_root = Path("/foss/pdks/sky130A/libs.tech/ngspice")
-sky130.install = sky130.Install(model_lib=sky130_pdk_root / "sky130.lib.spice")
+sky130_pdk_root = Path("/foss/pdks/sky130A")
+sky130.install = sky130.Install(
+    # pdk_path=Path(os.environ["PDK_ROOT"] + "/" + os.environ["PDK"]),
+    pdk_path=sky130_pdk_root,
+    lib_path=Path("libs.tech/ngspice/sky130.lib.spice"),
+    model_ref=Path("libs.ref/sky130_fd_pr/spice"),
+)
